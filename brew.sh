f6ced86d74bcd9085849171662fdfccf83a58268
David Otton <david@otton.org>
2024-04-26 12:49:32 +0100

    brew.sh Add packages I use

--- brew.sh
+++ brew.sh
@@ -102,5 +102,46 @@ brew install tree
 brew install vbindiff
 #brew install zopfli
 
+# My additions
+brew install adr-tools                 # Command-line tool for working with Architecture Decision Records https://github.com/npryce/adr-tools
+brew install alt-tab                   # Enable Windows-like alt-tab https://alt-tab-macos.netlify.app/
+brew install alure                     # Manage common tasks with OpenAL applications (required for bucklespring) https://kcat.tomasu.net/alure.html
+brew install curl                      # Get a file from an HTTP, HTTPS or FTP server https://curl.se/
+brew install ddev/ddev/ddev            # Containerized web development environments, defined in config. Supports PHP, Node.js, and Python (experimental). Requires docker or alternative. https://ddev.com/
+brew install dos2unix                  # Convert text between DOS, UNIX, and Mac formats https://waterlan.home.xs4all.nl/dos2unix.html
+brew install fdupes                    # Identify or delete duplicate files https://github.com/adrianlopezroche/fdupes
+brew install ffmpeg                    # Play, record, convert, and stream audio and video https://ffmpeg.org/
+brew install gh                        # GitHub command-line tool https://cli.github.com/
+brew install handbrake                 # Video transcoder https://handbrake.fr/
+brew install jq                        # Command-line JSON processor https://jqlang.github.io/jq/
+brew install lesspipe                  # Input filter for the pager less https://www-zeuthen.desy.de/~friebel/unix/lesspipe.html
+brew install mods                      # AI on the command-line https://github.com/charmbracelet/mods
+brew install pkg-config                # Manage compile and link flags for libraries (required for bucklespring) https://freedesktop.org/wiki/Software/pkg-config/
+brew install python                    # Python 3 https://www.python.org/
+brew install rogue                     # Dungeon crawling video game https://sourceforge.net/projects/roguelike/
+brew install rsync                     # Utility that provides fast incremental file transfer https://rsync.samba.org/
+brew install scrcpy                    # Display and control your Android device https://github.com/Genymobile/scrcpy
+brew install sevenzip                  # 7-Zip is a file archiver with a high compression ratio https://7-zip.org
+brew install sshuttle                  # Proxy server that works as a poor man's VPN https://github.com/sshuttle/sshuttle
+brew install tailscale                 # Easiest, most secure way to use WireGuard and 2FA https://tailscale.com/
+brew install telnet                    # User interface to the TELNET protocol
+brew install zed                       # Multiplayer code editor https://zed.dev/
+
+# brew install --cask whatsapp         # Native desktop client for WhatsApp https://www.whatsapp.com/
+# brew install mkcert                  # Simple tool to make locally trusted development certificates (ddev assumes this, but I try to make do without it because I consider it a security hole) https://github.com/FiloSottile/mkcert
+brew install --cask discord            # Voice and text chat software https://discord.com/
+brew install --cask disk-inventory-x   # Disk usage utility https://www.derlien.com/
+brew install --cask docker             # Docker Desktop (conflicts with docker formula) https://www.docker.com/products/docker-desktop/
+brew install --cask firefox            # Web browser https://www.mozilla.org/firefox/
+brew install --cask google-chrome      # Web browser https://www.google.com/chrome/
+brew install --cask iterm2             # Terminal emulator https://www.iterm2.com/
+brew install --cask musicbrainz-picard # Music tagger https://picard.musicbrainz.org/
+brew install --cask obsidian           # Knowledge base that works on top of a local folder of plain text Markdown files https://obsidian.md/
+brew install --cask phpstorm           # PHP IDE by JetBrains https://www.jetbrains.com/phpstorm/
+brew install --cask slack              # Team communication and collaboration software https://slack.com/intl/en-gb
+brew install --cask transmission       # BitTorrent client https://transmissionbt.com/
+brew install --cask vlc                # Multimedia player https://www.videolan.org/vlc/
+brew install --cask zoom               # Video communication and virtual meeting platform https://www.zoom.us/
+
 # Remove outdated versions from the cellar.
 brew cleanup
